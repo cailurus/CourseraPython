33d3611eb952882b32d547e4b4882820c5473215
jinyangzhou <pandachow19910411@gmail.com>
2013-05-18 07:04:23 +0800

    finished part 1
    name to number function

--- rpsls.py
+++ rpsls.py
@@ -1,2 +1,14 @@
 def name_to_number(name):
-	
+	if name == 'rock':
+		return 0
+	elif name == 'Spock':
+		return 1
+	elif name == 'paer':
+		return 2
+	elif name == 'lizard':
+		return 3
+	else:
+		return 4
+
+name1 = 'rock'
+print str(name_to_number(name1))

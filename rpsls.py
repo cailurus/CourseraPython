698f92f8287c8a638930fb1c1ea08c56feb3798e
jinyangzhou <pandachow19910411@gmail.com>
2013-05-18 07:09:01 +0800

    finished the second part
    number to name part

--- rpsls.py
+++ rpsls.py
@@ -3,12 +3,28 @@ def name_to_number(name):
 		return 0
 	elif name == 'Spock':
 		return 1
-	elif name == 'paer':
+	elif name == 'paper':
 		return 2
 	elif name == 'lizard':
 		return 3
 	else:
 		return 4
 
+
+def number_to_name(number):
+	if number == 0:
+		return 'rock'
+	elif number == 1:
+		return 'Spock'
+	elif number == 2:
+		return 'paper'
+	elif number == 3:
+		return 'lizard'
+	else:
+		return scissors
+	
+	
 name1 = 'rock'
+number1 = 3
 print str(name_to_number(name1))
+print number_to_name(number1)

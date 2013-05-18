70eb1be7eae985e3b226e511464d9471cbe5dc30
jinyangzhou <pandachow19910411@gmail.com>
2013-05-18 18:10:55 +0800

    correct to the class version
    use the simplegui module

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,36 +1,50 @@
 # template for "Guess the number" mini-project
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
+
+import simplegui
 import random
+import math
 
 # initialize global variables used in your code
-
+num_range = 100
 
 # define event handlers for control panel
-    
-#def range100():
-    # button that changes range to range [0,100) and restarts
 
-#def range1000():
+
+def range100():
+    global num_range
+    num_range = random.randrange(0,100)
+    return num_range
+    
+def range1000():
+    global  num_range
+    num_range = random.randrange(0,1000)
+    return num_range
+    
     # button that changes range to range [0,1000) and restarts
     
 def get_input(guess):
-	goal = random.randrange(0,100)
-	if(guess == goal):
-		print "My guess is "+str(guess)+", and the goal is "+str(goal)+", so it's correct!"
-	elif(guess > goal):
-		print "My guess is "+str(guess)+", and the goal is "+str(goal)+", so it's higher!"
-		get_input(guess)
-	else:
-		print "My guess is "+str(guess)+", and the goal is "+str(goal)+", so it's lower!"
-		get_input(guess)
-	
-get_input(40)
+    guess = int(guess)
+    if guess == num_range:
+        print "correct"
+    elif guess > num_range:
+        print "goal is "+str(num_range)+", current is "+str(guess)+",higher!"
+        guess = guess - 1
+        get_input(guess)
+    else:
+        print "goal is "+str(num_range)+", current is "+str(guess)+", lower!"
+        guess = guess + 1
+        get_input(guess)
+
     
 # create frame
-
+f = simplegui.create_frame("Guess the number", 200, 200)
 
 # register event handlers for control elements
+f.add_button("Range is [0,100)", range100, 200)
+f.add_button("Range is [0,1000)", range1000, 200)
+f.add_input("Enter a guess", get_input, 200)
 
 
 # start frame
